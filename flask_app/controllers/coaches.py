d94dadc846096831c828f0f3d2f48913f87901aa
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-03-22 12:58:37 -0400

    quick update for ERD standardization
    continuing POST CRUD

--- flask_app/controllers/coaches.py
+++ flask_app/controllers/coaches.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash, url_for
-from flask_app.models import coach, post, event, athlete
+from flask_app.models import coach, post, event, athlete, post
 from flask_app.controllers import comments, events, posts
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
@@ -15,7 +15,7 @@ def index():
 def dashboard():
     if 'coach_id' not in session:
         return redirect('/')
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', coach_posts=post.Post.get_all_logged_in_coaches_posts())
 
 @app.route('/coach/register',methods=['POST'])
 def coach_registration():
--- flask_app/controllers/posts.py
+++ flask_app/controllers/posts.py
@@ -5,4 +5,14 @@ from flask import render_template, redirect, request, session, flash, url_for
 from flask_app.models import coach, post, event
 from flask_app.controllers import coaches
 
+@app.route('/coach/create_post_page/<int:coach_id>')
+def create_post_page(coach_id):
+    return render_template('create_post.html', coach_id=coach_id)
+
+@app.route('/coach/create_post/<int:coach_id>',methods=['POST'])
+def create_post(coach_id):
+    post.Post.create_post(request.form)
+    print('tried to create post!')
+    return redirect('/dashboard')
+
 
