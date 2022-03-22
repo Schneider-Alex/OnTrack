486ecbc7ca3eff4a66a5cf2426353ca19816b8e8
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-03-21 22:05:11 -0500

    test commit

--- flask_app/models/time.py
+++ flask_app/models/time.py
@@ -1,3 +1,7 @@
 from flask_app.config.mysqlconnection import MySQLConnection, connectToMySQL
 from flask_app import app
 
+class Time:
+    db = 'on_track'
+    def __init__(self, data): 
+        self.id = data['id']
